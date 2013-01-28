8804168cbfb8312610c4869b40236e6c0950c4fd
Yew Long <yewlong42@gmail.com>
2013-01-28 15:51:07 -0500

    coded and tested parseArgs. added parseArgs error codes to option.h

--- src/option.h
+++ src/option.h
@@ -2,6 +2,10 @@
 /*Also, defines constants to use*/
 #ifndef OPTION_H
 #define OPTION_H
-enum option{ D_SEND, D_DAEMON, L_LOCAL, NONE, ERROR=-1, MAC_MISMATCH=62};
+enum option{ D_SEND, D_DAEMON, L_LOCAL, NONE, ERROR=-1, MAC_MISMATCH=62,
+    STRCHR_ERROR, NUM_ARGS_ERROR, MISSING_IPPORT_ERROR, UNKNOWN_OPT_ERROR,
+    PORT_ERROR};
 #define HMAC_LENGTH (256)
+#define DEBUG (1)
+#define DPRINT if(DEBUG)printf
 #endif
--- src/techutils.c
+++ src/techutils.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "option.h"
+#include <string.h>
 
 /*cryptographic*/
 int deriveKey(char* password, char* salt, int numIterations, 
@@ -27,13 +28,65 @@ int writeFile(char* fileName, char* outFile, char* mac){
 }
 
 /*misc*/
+/*ipAddress parameter can be NULL since techdec does not use it.*/
 int parseArgs(int argc, char** argv, 
 	      char** fileName, char** ipAddress, int* port){
+    /*for reference
+    "usage: techrypt < input file > [-d < IP-addr:port >][-l ]"
+    "usage: techdec < filename >  [-d < port >][-l] "
+    */
+
+    int isTechDec = (ipAddress == NULL);
+    int option;
+
+    if( (argc <= 1) ||  (argc > 4)){
+	return NUM_ARGS_ERROR;
+    }
+
+    /* argc = 2, 3, 4*/
+    *fileName = argv[1];
+    DPRINT("fileName: %s ", *fileName);
+
+    if(argc >= 3){
+
+	/*In case of daemon*/
+	if(strcmp("-d", argv[2]) == 0){
+	    option = ((isTechDec) ? D_DAEMON : D_SEND);
+	    if(argc == 4){ /*get ipaddr and port num*/
+		if(!isTechDec){
+		    char* delimPos = strchr(argv[3], ':');
+		    if(NULL == delimPos){ return STRCHR_ERROR;}
+		    *delimPos = '\0';
+		    *ipAddress = argv[3];
+		    *port = atoi((delimPos+ 1));
+		    DPRINT("ip: %s:%d ", *ipAddress, *port);
+		} else{
+		    *port = atoi(argv[3]);
+		    DPRINT("port: %d ", *port);
+		}
+		if(0 == *port){ return PORT_ERROR;}
+	    } else{
+		return MISSING_IPPORT_ERROR; 
+	    }
+
+	/*In case of local*/
+	} else if(strcmp("-l", argv[2]) == 0){
+	    option = L_LOCAL;
+	    if(argc != 3){
+		return ERROR;
+	    }
+	} else{
+	    return UNKNOWN_OPT_ERROR;
+	}
+    } else if(argc == 2){
+	option = L_LOCAL; /* this is implied*/
+    }
+
     return NONE;
 }
 
 int checkErr(int err, char* msg){
-    if(ERROR == err){
+    if(NONE != err){
 	printf("%s\n", msg);
 	exit(err);
     }
